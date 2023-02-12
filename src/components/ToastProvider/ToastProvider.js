afee401ed07492e8742c156e93e337dc77c486d7
Ardis Lu <ardisglu@gmail.com>
2023-02-11 16:46:14 -0800

    Add global listener to dismiss all toasts with Esc

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,18 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    const handleKeydown = e => {
+      if (e.key === 'Escape') {
+        setToasts([]);
+      }
+    }
+    window.addEventListener('keydown', handleKeydown);
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, []);
+
   function addToast(message, variant = 'notice', id = crypto.randomUUID()) {
     const newToast = {
       message,

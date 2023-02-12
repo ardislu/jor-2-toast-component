5b8850e1a36dea62703a77f04d9d3863fa3f3099
Ardis Lu <ardisglu@gmail.com>
2023-02-11 17:16:58 -0800

    Remove unnecessary brackets

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -26,7 +26,7 @@ function Toast({ message, variant = 'notice', id }) {
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        {<Icon size={24} />}
+        <Icon size={24} />
       </div>
       <p className={styles.content}>
         <VisuallyHidden>{variant} -</VisuallyHidden>
